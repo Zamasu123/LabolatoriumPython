500b198d57f621dc4c9b28cc536adc21f8abbb95
w69028 <zamasu12363@gmail.com>
2023-03-17 14:06:14 +0100

    ..

---
+++ lab 2 zad 1.py
@@ -0,0 +1,23 @@
+import random
+
+def dind_min_elem(dane):
+    min = dane(0)
+    for i in dane:
+        if i<min:
+            min = i
+    return min
+
+def find_poz_min_elem(dane):
+    min_poz=0
+    for i in range(1, n):
+        if dane(i)<dane[min_poz]:
+            min_poz=i
+        return min_poz
+
+
+
+L = []
+for i in range(20):
+    lista.append(random.randint(10, 50))
+print(L)
+print("Najmniejszy element listy to: ",find_min_elem(L))
