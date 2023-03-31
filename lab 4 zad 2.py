320939b537951a0ba1ae986a48dc4c1c11608b5c
w69028 <zamasu12363@gmail.com>
2023-03-31 15:03:49 +0200

    a

--- lab 4 zad 2.py
+++ lab 4 zad 2.py
@@ -11,7 +11,19 @@ def nwd_r(a, b):
 
 def nwd_12(a, b):
     while b!=a:
-
+        if a>b:
+            a=a-b
+        else:
+            b=b-a
     return a
+def nwd_r2(a, b):
+    if a==b:
+        return a
+    elif a>b:
+        return nwd_r2(a-b, b)
+    else:
+        return nwd_r2(a, b-a)
+print(nwd_r2(24, 36))
+print(nwd_12(24, 36))
 print(nwd_r(24, 36))
 print(nwd_i(24, 36))
